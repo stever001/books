3188ac3a34e7d0d8f3fc7dee7baa86348468ca38
SteveR <steve17rouse@gmail.com>
2024-03-29 14:29:42 -0600

    addressed middleware refactor

--- Develop/server/utils/auth.js
+++ Develop/server/utils/auth.js
@@ -1,39 +1,46 @@
 const jwt = require('jsonwebtoken');
 
-// set token secret and expiration date
+// Set token secret and expiration date
 const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
-  // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+  // Function to be used as middleware in the context of Apollo Server
+  authMiddleware: function ({ req }) {
+    // New context for storing user data
+    let authToken = null;
+    let user = null;
+
+    // Allows token to be sent via req.body, req.query, or headers
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
       token = token.split(' ').pop().trim();
     }
 
+    // if no token, return null user in the context
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      return { user };
     }
 
-    // verify token and get user data out of it
+    // Verify token and get user data out of it
     try {
+      // we don't need to send the error response like before,
+      // just assign the user data to context or null if no valid token
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
-      req.user = data;
+      user = data;
     } catch {
       console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
+      user = null;
     }
 
-    // send to next endpoint
-    next();
+    // Return the new context with the user and token
+    return { user, authToken: token };
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
-
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 };
+
