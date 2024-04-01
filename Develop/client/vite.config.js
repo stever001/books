dd27179fa6a56e7b4ce2396ba3a70eca2f770a80
SteveR <steve17rouse@gmail.com>
2024-03-31 20:24:13 -0600

    updated port.

--- Develop/client/vite.config.js
+++ Develop/client/vite.config.js
@@ -5,11 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: 5001,
     open: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'http://localhost:5001',
         secure: false,
         changeOrigin: true
       }
