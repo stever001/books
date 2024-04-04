23c6b311a1689620df3ca1aa0ffdd4849766d360
SteveR <steve17rouse@gmail.com>
2024-04-03 18:56:03 -0600

    adjusted to apply to Render.

--- server/server.js
+++ server/server.js
@@ -24,7 +24,10 @@ async function startServer() {
 
   // Serve up static assets
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.use(express.static(path.join(__dirname, '../client/dist')));
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    });
   }
 
   db.once('open', () => {
